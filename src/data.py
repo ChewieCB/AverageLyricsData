d47a4e5fd1e1e59592ca504e0fa4d77d23fc208c
Jack <jackmccaffrey96@gmail.com>
2022-01-14 16:54:32 +0000

    -feat: Added `Artist`, `Release`, and `Track` classes to better structure and store data obtained from API requests.

---
+++ src/data.py
@@ -0,0 +1,55 @@
+
+
+class Artist:
+    def __init__(self, name: str, mb_id: str):
+        """
+
+        :param name: Full or official name of the artist returned by the API search.
+        :param mb_id: Artist id string used by the musicbrainz API for lookups.
+        """
+        self.name: str = name
+        self.mb_id: str = mb_id
+        self.releases: [Release] = []
+
+
+class Release:
+    def __init__(self, name: str, mb_id: str, year: str, type: str, artist: Artist):
+        """
+
+        :param name:
+        :param mb_id:
+        :param artist:
+        :param year:
+        """
+        self.name = name
+        self.mb_id = mb_id
+        self.artist = artist
+        self.year = year
+        self.type = type
+
+    def __str__(self):
+        return f"{self.year}: {self.name} ({self.type})"
+
+    def __repr__(self):
+        return f"{self.year}: {self.name} ({self.type})"
+
+
+class Track:
+    def __init__(self, name: str, mb_id: str, release: str):
+        """
+
+        :param name:
+        :param mb_id:
+        :param release:
+        """
+        self.name = name
+        self.mb_id = mb_id
+        self.release = release
+        self.lyrics: str
+
+    def __str__(self):
+        return f"{self.release}: {self.name}"
+
+    def __repr__(self):
+        return f"{self.release}: {self.name}"
+
