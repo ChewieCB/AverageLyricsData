1f4fde104f014ed115e73c224d5074c9220ee41f
Jack <jackmccaffrey96@gmail.com>
2022-01-17 12:24:15 +0000

    -feature: Improved filtering capability of `remove_duplicate_recordings()`.
    Started abstracting each type of check this method is performing into helper functions to improve readability, some errant recordings are slipping past the API query despite specifying an artist ID so added a sanity check to prune out any non-artist songs that get through. Still some issues with filtering out instrumentals on this, will re-visit at a later time.

--- src/main.py
+++ src/main.py
@@ -16,17 +16,18 @@ async def main():
         artist_id_url = api_parser.build_artist_search_query_url(artist_name_query)
         artist = await get_artist_data(session, artist_id_url)
 
-        # Get the recording data from the API using the artist ID
-        recordings = await get_recordings_data(session, artist)
         print("=" * 32)
         print(f"{artist.name}")
         print("=" * 32)
-        print(f"{len(recordings)} tracks.\n")
-        # for track in recordings:
-        #     print(track)
+
+        # Get the recording data from the API using the artist ID
+        recordings = await get_recordings_data(session, artist)
+        print(f"\n{len(recordings)} tracks.\n")
 
         # Clean the data by removing any duplicate songs/singles/remixes/re-releases/etc.
-        _cleaned_recordings = remove_duplicate_recordings(recordings)
+        cleaned_recordings = remove_duplicate_recordings(recordings, artist)
+        for track in cleaned_recordings:
+            print(track)
 
 
 async def get_artist_data(session: aiohttp.ClientSession, url: str) -> Artist:
@@ -48,54 +49,16 @@ async def get_artist_data(session: aiohttp.ClientSession, url: str) -> Artist:
     # Assign the data from the API into an Artist class for later reference
     artist_name = artist_data["artists"][0]["name"]
     artist_id = artist_data["artists"][0]["id"]
-    artist_object = Artist(name=artist_name, mb_id=artist_id)
+    artist_object = Artist(raw_data=artist_data, name=artist_name, mb_id=artist_id)
+    print(artist_id)
 
     return artist_object
-
-
-# async def get_release_data(session: aiohttp.ClientSession, url: str, artist: Artist) -> [Release]:
-#     """
-#
-#     :param session:
-#     :param url:
-#     :param artist:
-#     :return:
-#     """
-#     async with session.get(url) as response:
-#         print(response.status)
-#         release_data = await response.json()
-#
-#         # If we get no data from the API, show the user an error message and exit.
-#         if not release_data or len(release_data["release-groups"]) < 0:
-#             print("No releases found!")
-#             return
-#
-#     # Assign the data from the API into Release objects for later reference
-#     releases = []
-#     for _release in release_data["release-groups"]:
-#         release_id = _release.get("id")
-#         release_title = _release.get("title")
-#         release_type = _release.get("primary-type")
-#         release_year = _release.get("first-release-date")
-#
-#         release_object = Release(
-#             name=release_title,
-#             mb_id=release_id,
-#             year=release_year,
-#             type=release_type,
-#             artist=artist,
-#         )
-#         releases.append(release_object)
-#         print(release_object)
-#
-#     return releases
 
 
 async def get_recordings_data(session: aiohttp.ClientSession, artist: Artist) -> [Track]:
     """
 
     :param session:
-    :param url:
     :param artist:
     :return:
     """
@@ -111,6 +74,7 @@ async def get_recordings_data(session: aiohttp.ClientSession, artist: Artist) ->
     # While the tracks retrieved is less than the number of tracks in the list, add an offset and make another request
     while track_count == 0 or tracks_retrieved < track_count:
         # Build the url
+        # FIXME - not getting all album data for some reason? Alpha/Omega don't show up :/
         recordings_url = api_parser.build_recordings_lookup_url(artist.mb_id, tracks_retrieved)
 
         # Make a query request to the API
@@ -130,7 +94,9 @@ async def get_recordings_data(session: aiohttp.ClientSession, artist: Artist) ->
             _title = track.get("title")
             _release_title = track.get("releases")[0].get("title")
             _release_type = track.get("releases")[0].get("release-group").get("primary-type")
+
             current_track = Track(
+                raw_data=track,
                 name=_title,
                 mb_id=_id,
                 release=_release_title,
@@ -141,7 +107,7 @@ async def get_recordings_data(session: aiohttp.ClientSession, artist: Artist) ->
     return recordings
 
 
-def remove_duplicate_recordings(raw_recordings_data: [Track]) -> Track:
+def remove_duplicate_recordings(raw_recordings_data: [Track], artist: Artist) -> Track:
     """
     Go through each recording from the data and remove any duplicate tracks.
     :param raw_recordings_data: A list of Track objects.
@@ -153,15 +119,28 @@ def remove_duplicate_recordings(raw_recordings_data: [Track]) -> Track:
     # Hacky shit way of getting this to work, loop through each element and check if any other songs have the element
     # name as a substring, optimise later - bunch of issues with this buy lets make a start at least.
     for track in local_recording_data:
+        # Quick sanity check to see if a track with the wrong artist ID has slipped through the search filters
+        if is_non_artist_song(track, artist):
+            print(f"{track} is not by the artist {artist.name} - removing.")
+            local_recording_data.remove(track)
+            continue
+
+        # Are the words "live", "remix", or "instrumental" in the name? Remove the track.
+        # FIXME - still not getting all of them, few DGD instrumental albums slipping through
+        if is_re_release_or_instrumental(track, artist):
+            print(f"Removing {track}! as it is likely a remix, instrumental, or live version.")
+            local_recording_data.remove(track)
+            continue
+
         # Check if the track name appears as a sub string in any other track
         for other_track in local_recording_data:
+            # Don't check against yourself
             if local_recording_data.index(other_track) == local_recording_data.index(track):
                 continue
+
+            # If we have an exact match it's likely a single or EP re-release, in this case we prioritise album tracks
             elif other_track.name == track.name:
-                # print("\nDuplicate: same name!")
-                # print(f"track = {track} ; other track = {other_track}")
-                # Determine which one to remove
-                #
+                # Determine which one to remove:
                 # Is one a single with the same name? Remove that one.
                 if other_track.release_type == "Single":
                     print(f"Removing album single: {other_track}")
@@ -169,33 +148,42 @@ def remove_duplicate_recordings(raw_recordings_data: [Track]) -> Track:
                 elif other_track.release_type == "EP":
                     print(f"Removing re-released EP track: {other_track}")
                     local_recording_data.remove(other_track)
-            elif other_track.name.find(track.name) != -1:
-                # Are the words "live", "remix", or "instrumental" in the name? Remove that one.
-                # TODO - add cli flag to allow remixes in the counting
-                for keyword in ["live", "mix", "deluxe", "instrumental", "session"]:
-                    if keyword in other_track.name.lower() or keyword in other_track.release.lower():
-                        # print("\nDuplicate: sub string in name!")
-                        # print(f"Duplicate: track = {track} ; other track = {other_track}")
-                        print(f"Removing {other_track}! as it is likely a remix, instrumental, or live version.")
-                        local_recording_data.remove(other_track)
-                        break
-                # # Edge case detection here, i.e. Zero is NOT a dupe of Mile Zero - they're both
-                # # tracks on different albums.
-                # if track.release != other_track.release and track.release_type != "Single" and other_track.release_type != "Single":
-                #     print(f"Not removing, tracks are not singles and are on different releases.")
 
-    print("\n")
-    for track in local_recording_data:
-        print(track)
-
-    print("\n")
     print("=" * 32)
     print(f"Original tracklist length = {original_length}")
     new_length = len(local_recording_data)
     print(f"New tracklist length = {new_length}")
     print(f"Duplicate tracks removed = {original_length - new_length}")
+    print("=" * 32)
 
-    return []
+    return local_recording_data
+
+
+def is_non_artist_song(track: Track, artist: Artist) -> bool:
+    """
+    Helper method to detect songs not by the chosen artist that have slipped through the API search filter.
+    :param track:
+    :param artist:
+    :return:
+    """
+    track_artist_id = track.raw_data.get("artist-credit")[0].get("artist").get("id")
+    if track_artist_id != artist.mb_id:
+        return True
+    return False
+
+
+def is_re_release_or_instrumental(track: Track, artist: Artist) -> bool:
+    """
+    Helper method to detect songs re-released as live or remixed versions,
+    and instrumental tracks we don't need to find lyrics for.
+    :param track:
+    :param artist:
+    :return:
+    """
+    for keyword in ["live", "mix", "deluxe", "instrumental", "session"]:
+        if track.name.lower().find(keyword) != -1 or track.release.lower().find(keyword) != -1:
+            return True
+    return False
 
 
 if __name__ == "__main__":
