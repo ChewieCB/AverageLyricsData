e07ffb79fb19470bee1d64420d48e4f8514c4080
Jack <jackmccaffrey96@gmail.com>
2022-01-27 18:09:22 +0000

    -style: Removed redundant brackets around return statements and renamed `input` arg to `input_string` to be more descriptive and avoid namespace clashes.

--- helpers/output_helpers.py
+++ helpers/output_helpers.py
@@ -1,5 +1,3 @@
-from sys import stderr
-from dataclasses import dataclass
 
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
@@ -12,41 +10,41 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
 
-def wrap_text(input: str, colour) -> str:
-    """Given a string, wrap the input with a header and enc character to produce a formatted header string."""
-    return colour + input + ENDC
+def wrap_text(input_string: str, colour) -> str:
+    """Given a string, wrap the input_string with a header and enc character to produce a formatted header string."""
+    return colour + input_string + ENDC
 
 
-def header(input: str) -> str:
-    return(wrap_text(input, HEADER))
+def header(input_string: str) -> str:
+    return wrap_text(input_string, HEADER)
 
 
-def blue(input: str) -> str:
-    return(wrap_text(input, OKBLUE))
+def blue(input_string: str) -> str:
+    return wrap_text(input_string, OKBLUE)
 
 
-def cyan(input: str) -> str:
-    return(wrap_text(input, OKCYAN))
+def cyan(input_string: str) -> str:
+    return wrap_text(input_string, OKCYAN)
 
 
-def green(input: str) -> str:
-    return(wrap_text(input, OKGREEN))
+def green(input_string: str) -> str:
+    return wrap_text(input_string, OKGREEN)
 
 
-def warning(input: str) -> str:
-    return(wrap_text(input, WARNING))
+def warning(input_string: str) -> str:
+    return wrap_text(input_string, WARNING)
 
 
-def fail(input: str) -> str:
-    return(wrap_text(input, FAIL))
+def fail(input_string: str) -> str:
+    return wrap_text(input_string, FAIL)
 
 
-def bold(input: str) -> str:
-    return(wrap_text(input, BOLD))
+def bold(input_string: str) -> str:
+    return wrap_text(input_string, BOLD)
 
 
 def separator(size: int = 128, single: bool = False) -> str:
     if single:
-        return("-" * size)
+        return "-" * size
     else:
-        return("=" * size)
+        return "=" * size
