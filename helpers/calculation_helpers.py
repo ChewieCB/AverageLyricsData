6230008fccef2e1de46e435136a1a2bc3b49c3dd
Jack <jackmccaffrey96@gmail.com>
2022-01-28 18:27:03 +0000

    -refactor: Improved plot clarity by limiting x-ticks, displaying only the year of the release date, and adding the average as a dashed line.
    Also added docstrings to `calculation_helpers` methods.

--- helpers/calculation_helpers.py
+++ helpers/calculation_helpers.py
@@ -7,7 +7,14 @@ import helpers.output_helpers as oh
 
 
 def calculate_output(recordings_with_lyrics: [Track], artist: Artist) -> (int, str):
-    """"""
+    """
+    For a list of cleaned Track objects with lyrics, calculate the average number of words
+    used by the Track's Artist, as well as other statistical values such as standard deviation,
+    variance, minimum and maximum values.
+    :param recordings_with_lyrics: A list of Track objects with cleaned lyrics attributes.
+    :param artist: The Artist object linked to the tracks.
+    :return: A tuple containing the average word count as an integer and a string to pass as an error message.
+    """
     # Error handling
     if not recordings_with_lyrics:
         return None, oh.fail("No lyrics to count!")
@@ -40,8 +47,16 @@ def calculate_output(recordings_with_lyrics: [Track], artist: Artist) -> (int, s
     return average_word_count, None
 
 
-def plot_data(track_data: [Track], artist: Artist) -> None:
-    """"""
+def plot_data(track_data: [Track], average_word_count: int, artist: Artist) -> None:
+    """
+    Given the cleaned and calculated track data plot a scatter graph of an Artist's songs, with the
+    year of release along the x-axis, the number of words in the track along the y-axis, and the
+    average number of words plotted as a dashed line across the plot.
+    :param track_data: Cleaned list of Track objects.
+    :param average_word_count: The average number of words across all tracks.
+    :param artist: The Artist object linked to the tracks.
+    :return: None.
+    """
     xs = []
     ys = []
 
@@ -51,20 +66,23 @@ def plot_data(track_data: [Track], artist: Artist) -> None:
         reverse=False,
     )
 
-    # for track in tracks_with_lyrics_year_ascending:
-    #     print(track.name, track.release.date)
+    # Extract only the years from each track's release date, some tracks only have a year value
+    # but all date values should follow a %Y-%m-%d format so we can just slice the first 4 chars
+    # to get the year in this case.
+    for track in tracks_with_lyrics_year_ascending:
+        xs.append(track.release.date[0:4])
 
     for point in tracks_with_lyrics_year_ascending:
-        xs.append(point.release.date)
         ys.append(point.word_count)
-
-    # TODO - Format x-ticks to use only the year value
-    # from datetime import datetime
-    # print(xs)
-    # try:
-    #     year_xticks = [datetime.strptime(item, "%Y-%m-%d").year for item in xs]
 
     fig, ax = plt.subplots()
     ax.set_title(f"Word count of {artist.name} songs over time.")
+
+    # Limit the number of x-ticks so we don't get flooded with date labels
+    # and obscure the x-axis for large datasets
+    ax.xaxis.set_major_locator(plt.MaxNLocator(12))
+
     plt.scatter(xs, ys)
+    # Plot the average as a dashed black line
+    plt.plot([xs[0], xs[-1]], [average_word_count, average_word_count], 'k--')
     plt.show()
